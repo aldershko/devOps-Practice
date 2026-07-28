0830abdd046548bd29b6cd2fe983172ad681319f
Alen <alen@AlenCMF.localdomain>
2026-07-28 13:23:11 +0000

    Accidently break the logic

--- readjson_logic.py
+++ readjson_logic.py
@@ -3,6 +3,6 @@ import json
 def find_stopped_servers(servers):
 	stopped_servers = []
 	for server in servers:
-		if server["status"] == "stopped":
+		if server["status"] == "Stopped":
 			stopped_servers.append(server["server_name"])
 	return stopped_servers
